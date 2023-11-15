b480bcbf55001fff8ed92a0d3fd553518d081146
Charlène <charlene.morchipont@etu.umontpellier.fr>
2023-11-15 11:04:20 +0100

    Les modèles

--- src/Models/JeuEspace.js
+++ src/Models/JeuEspace.js
@@ -1,12 +1,24 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database'); 
 
-const JeuEspace = sequelize.define('JeuEspace', {
-  
-}, {
-  tableName: 'jeu_espace',
-  timestamps: false,
-});
+const JeuEspace = sequelize.define(
+  'JeuEspace',
+  {
+    idJeu: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    idZoneBenevole: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  },
+  {
+    tableName: 'jeu_espace',
+    timestamps: false,
+  }
+);
 
 
 module.exports = JeuEspace;
+
