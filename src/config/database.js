4376171b561d8ea0d58a8f357a0fe7b9a02c4f31
Jiayi HE <he_jia_yi1995@126.com>
2024-02-05 20:01:22 +0100

    url

--- src/config/database.js
+++ src/config/database.js
@@ -3,18 +3,29 @@ const { Sequelize } = require("sequelize")
 
 //const username = process.env.DB_USERNAME || "postgres";
 //const password = process.env.DB_PASSWORD || "123";
-c//onst database = process.env.DB_NAME || "benevoleAppDB";
+//onst database = process.env.DB_NAME || "benevoleAppDB";
 
 const username = process.env.DB_USERNAME || "benevole_app_bd_user";
 const password = process.env.DB_PASSWORD || "9t2HHayiIYldQqeah6Vf3as80UepU7g4";
 const database = process.env.DB_NAME || "benevole_app_bd";
+const host = process.env.DB_HOST || "benevole-app-bd.postgres.database.azure.com";
 
 
+{/*
 const sequelize = new Sequelize(database, username, password, {
   host: 'localhost',
   dialect: 'postgres',
   logging: false,
 });
+*/}
+
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'postgres',
+  logging: false,
+});
+
+
 
 
 
