6429708416dd66ab60767d751f0798bde8346d0a
Jiayi HE <he_jia_yi1995@126.com>
2024-02-05 20:10:21 +0100

    url

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,23 +3,12 @@ const{ sign, verify } = require('jsonwebtoken')
 
 
 const validateToken = (req, res, next)=>{
-    try{
-        
-        const accessToken = req.headers.authorization.split(' ')[1];
-<<<<<<< HEAD
-        console.log("coucou")
-        console.log(req.headers.authorization)
-        console.log("coucou2")
-        console.log(req.headers.authorization.split(' '))
-=======
-        console.log(req.headers.authorization.split(' '))
-        console.log(jwt.decode(accessToken));
 
->>>>>>> 48b0311ab4235176dc2a02034facfd66f65bf41f
+    try{
+        const accessToken = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(accessToken, "jwtSecret");
 
         console.log(decodedToken)
-
 
         const iduser = decodedToken.iduser; 
 
@@ -30,7 +19,6 @@ const validateToken = (req, res, next)=>{
         
         next();
     } catch (err){
-        console.error("Error verifying token:", err);
         return res.status(401).json({error: err});
     }
 }
