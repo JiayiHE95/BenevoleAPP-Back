fd8a58adb35b20148e0b0525d148b32658da18f4
Jiayi HE <he_jia_yi1995@126.com>
2024-03-13 18:27:26 +0100

    dev mobile log

--- src/Routes/FestivalRoutes.js
+++ src/Routes/FestivalRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/get-festival-by-annee/:annee', validateToken, festivalController.getFestivalByAnnee)
 router.post('/create', validateToken,festivalController.createFestival)
 router.post('/delete', validateToken,festivalController.deleteFestival)
-router.get('/all', validateToken,festivalController.getAllFestivals)
+router.get('/all', festivalController.getAllFestivals)
 router.get('/:id', validateToken,festivalController.getFestivalSelection)
 router.post('/update', validateToken,festivalController.updateFestival)
 
